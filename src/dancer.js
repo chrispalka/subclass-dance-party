df4f8c28ce1255ef4aa6a6600b31183fb8c583e1
Chris Palka <cpalka87gmail.com>
2020-04-23 15:20:54 -0600

    Add link to add dancer and add blinkydancer object

--- src/dancer.js
+++ src/dancer.js
@@ -5,20 +5,17 @@ var MakeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
-  //this.setPosition(top, left);
+  this.setPosition(top, left);
 };
 
-//MakeDancer.step();
 
 MakeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   // setTimeout(this.step.call(this), this.timeBetweenSteps);
-  setTimeout(this.step, this.timeBetweenSteps);
-  console.log('step invoked');
+  
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
-
-//console.log(this.step);
 
 MakeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
@@ -33,4 +30,3 @@ MakeDancer.prototype.setPosition = function(top, left) {
 // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
 // this one sets the position to some random default point within the body
 
-// dancer.setPosition(top, left);
