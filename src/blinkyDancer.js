54ad72f682283d921fc26458651d6b874c740fdb
Amanda Winkles <amandawinkles@gmail.com>
2020-04-23 16:05:39 -0500

    changed call to bind and defined step as oldStep, then called

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,14 +1,16 @@
 var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
-  this.callCount = 0;
+  //this.callCount = 0;
+  //this.$node.append(); //add img tag w/src, alt, and class in string
 };
 
 MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
 MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
 
 MakeBlinkyDancer.prototype.step = function() {
-  this.callCount++;
-  MakeDancer.prototype.step.call(this);
+  //this.callCount++;
+  var oldStep = MakeDancer.prototype.step.bind(this);
+  oldStep();
   this.$node.toggle();
 };
 
