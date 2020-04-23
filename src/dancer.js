e47091007f5da0b181a4c8c163df0f56425605e8
Amanda Winkles <amandawinkles@gmail.com>
2020-04-23 15:57:07 -0500

    re-added bind for this and added this for styleSettings in setPosition

--- src/dancer.js
+++ src/dancer.js
@@ -5,17 +5,15 @@ var MakeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
-  //this.setPosition(top, left);
+  //this.setPosition();
 };
-
-//MakeDancer.step();
 
 MakeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   // setTimeout(this.step.call(this), this.timeBetweenSteps);
-  setTimeout(this.step, this.timeBetweenSteps);
-  console.log('step invoked');
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  //console.log('step invoked- this: ', this);
 };
 
 //console.log(this.step);
@@ -24,9 +22,12 @@ MakeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   var styleSettings = {
-    top: top,
-    left: left
+    top: this.top,
+    left: this.left
   };
+
+  //lineUp function
+
   this.$node.css(styleSettings);
 };
 
